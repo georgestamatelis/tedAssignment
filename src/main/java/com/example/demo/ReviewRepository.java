046b46ed694f7f60e3f1bcfb2735768755bd55b5
georgestamatelis <52538570+georgestamatelis@users.noreply.github.com>
2020-04-04 00:04:34 +0400

    Add files via upload

---
+++ src/main/java/com/example/demo/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ReviewRepository extends CrudRepository<Review, Integer> {
+    List<Review> findAllByUserName(String UserName);
+    List<Review> findAllByAppId(Integer appId);
+}
